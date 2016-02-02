a9942683c1e59c140aac6d0b745f4ce9790ea6ca
mfrancis95 <mikefrancis95@gmail.com>
2016-02-02 10:23:08 -0500

    Remove need for cheerio by making base64ifyHTML() use string replacement

--- base64ify.js
+++ base64ify.js
@@ -1,31 +1,16 @@
-var cheerio = require("cheerio");
 var fs = require("fs");
 var mime = require("mime-types");
 
-function base64ifyHTML(html, callback) {
-    var elements = 0, elementsDone = 0;
-    var $ = cheerio.load(html);
-    var images = $("img");
-    elements += images.length;
-    if (elements === 0) {
-        if (callback) {
-            callback(html);
+function base64ifyHTML(html) {
+    return html.replace(/src=".*(?=")/g, function(match) {
+        var file = match.slice(5);
+        try {
+            return "src=\"data:" + mime.lookup(file) + ";base64," + fs.readFileSync(file).toString("base64");
         }
-    }
-    else {
-        images.each(function() {
-            var image = $(this);
-            var src = image.attr("src");
-            fs.readFile(src, function(error, data) {
-                if (!error) {
-                    image.attr("src", "data:" + mime.lookup(src) + ";base64," + data.toString("base64"));
-                }
-                if (++elementsDone === elements) {
-                    callback($.html());
-                }
-            });
-        });
-    }
+        catch (e) {
+            return match;
+        }
+    });
 }
 
 function base64ifyCSS(css) {
