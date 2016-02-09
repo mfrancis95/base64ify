55923cf2f38af30199ae43b320285fafdcc9b974
mfrancis95 <mikefrancis95@gmail.com>
2016-02-09 10:57:29 -0500

    Support src attributes with single quotes

--- base64ify.js
+++ base64ify.js
@@ -1,18 +1,19 @@
 var mime = require("mime-types");
 var fs = require("fs");
 
+var srcRegex = /src=('|").*(?='|")/g;
+var urlRegex = /url\(.*(?=\))/g;
+
 function base64ifyHTML(html, blacklist) {
     blacklist = (blacklist || []).map(mime.lookup);
-    return html.replace(/src=".*(?=")/g, function(match) {
+    return html.replace(srcRegex, function(match) {
         try {
             var file = match.slice(5);
             var mimeType = mime.lookup(file);
             if (blacklist.indexOf(mimeType) >= 0) {
                 return match;
             }
-            else {
-                return "src=\"data:" + mimeType + ";base64," + fs.readFileSync(file).toString("base64");
-            }
+            return "src=" + match[4] + "data:" + mimeType + ";base64," + fs.readFileSync(file).toString("base64");
         }
         catch (e) {
             return match;
@@ -22,19 +23,19 @@ function base64ifyHTML(html, blacklist) {
 
 function base64ifyCSS(css, blacklist) {
     blacklist = (blacklist || []).map(mime.lookup);
-    return css.replace(/url\(.*(?=\))/g, function(match) {
+    return css.replace(urlRegex, function(match) {
         try {
             var file = match.slice(4);
             var mimeType = mime.lookup(file);
             if (blacklist.indexOf(mimeType) >= 0) {
                 return match;
             }
-            else {
-                return "url(data:" + mimeType + ";base64," + fs.readFileSync(file).toString("base64");
-            }
+            return "url(data:" + mimeType + ";base64," + fs.readFileSync(file).toString("base64");
         }
         catch (e) {
             return match;
         }
     });
 }
+
+console.log(base64ifyCSS(fs.readFileSync("test.css").toString()));
